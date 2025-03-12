a103e2f50344bbeb15aac769e60f256846cc0814
kevinFichou <66632429+kemrode@users.noreply.github.com>
2025-03-12 14:58:40 +0100

    feat(master): add extension for the sum of n complexes

--- src/main/scala/Complexe.scala
+++ src/main/scala/Complexe.scala
@@ -1,17 +1,16 @@
 class Complexe(val entier: Int, val imaginaire: Int) {
     override def toString(): String = {
         (this.entier, this.imaginaire) match
+            case (0,0) => "O"
             case (0,_) => s"${this.imaginaire}i"
             case (_,0) => s"${this.entier}"
             case (_,_) => s"${this.entier} + ${this.imaginaire}i"
-            case (0,0) => "O"
-    }  
-
+    }
+ 
     def Créer(): String = {
         val complexe = s"${entier} + ${imaginaire}i"
         complexe
     }
-
 
     def +(complexe2: Complexe): Complexe = {
         val complexe = Complexe((this.entier + complexe2.entier), (this.imaginaire + complexe2.imaginaire))
@@ -21,4 +20,13 @@ class Complexe(val entier: Int, val imaginaire: Int) {
 
 object Complexe {
     def apply(entier1: Int, entier2: Int): Complexe = new Complexe(entier1, entier2)
+
+    def Somme(complexes: List[Complexe]): Complexe = {
+        complexes.reduce((acc,c) => acc + c )
+    } 
+
+    extension (start: List[Complexe])
+        def summe(): Complexe = {
+            Somme(start)
+        } 
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,14 @@
+import Complexe.Somme
 @main def hello(): Unit =
   val complexe1: Complexe = Complexe(2, 3)
   val complexe2: Complexe = Complexe(5,9)
   val resultat = AdditionDeuxComplexes(complexe1, complexe2)
-  println(resultat)
+  println(s"Le calcul de la somme de deux complexes vaut: ${resultat}")
+
+  val complexe3: Complexe = Complexe(1,10)
+  val complexes: List[Complexe] = List(complexe1, complexe2, complexe3)
+  val calculSuite = Somme(complexes)
+  println(s"Le résulat de la somme d'une suite de complexe vaut: $calculSuite")
 
 def AdditionDeuxComplexes(complexe1: Complexe, complexe2: Complexe): Complexe = {
   val complexeCalcule: Complexe = complexe1 + complexe2
